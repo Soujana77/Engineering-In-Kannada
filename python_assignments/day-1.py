c4508c19f1667a405f54cae7aa7b4e4c1ea35a53
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-09-16 21:25:25 +0530

    Completd day-1 assignment

---
+++ python_assignments/day-1.py
@@ -0,0 +1,9 @@
+'''Homework
+Download and install Python on your system. - Reference Video
+If you don't have a laptop, Install this app - Python Code-Pad
+Write your first Python program to print your name.
+print("Namaste, nanna hesaru [Your Name]!")
+Practice running your Python code through both an IDE and a terminal.
+'''
+
+print("Namaste, nanna hesaru Soujanya Jain!")
