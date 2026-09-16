d53af46d749d89651d9f048a48aa704ec510cfa6
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-09-16 21:36:38 +0530

    Completd day-2 assignment

---
+++ python_assignments/day-2.py
@@ -0,0 +1,23 @@
+'''
+1.Write a python python program that perfrorms basic airthematic operations
+(addition,subtraction,multiplication,division)
+on two numbers . Define the two numbers as variables within the code 
+and print the results for each operation.
+'''
+a = 10 
+b = 7
+print("sum of a and b is:", a+b)
+print("difference of a and b is:", a-b)
+print("product of a and b is:", a*b)
+print("quotient of a and b is:", a/b)
+
+'''
+2.Swap two variables : Write a python program to swap two variables
+ without using a third variable. Define the two variables within the code and print the values before and after swapping.
+'''
+
+x = 90
+y = 70
+
+print("before swapping:x=",x,"y=",y)
+print("after swapping:x=",y,"y=",x)
